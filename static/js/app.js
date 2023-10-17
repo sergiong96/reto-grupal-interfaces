8815d85ef895c60001572dae31a4e9f683cd9e3e
sergiong96 <serfer65@gmail.com>
2023-10-17 11:27:02 +0200

    Añadir entrada y borrar registros realizado en portal alumno

--- static/js/app.js
+++ static/js/app.js
@@ -27,15 +27,56 @@ function validarAlumno() {
 
 function añadirEntradaAlu() {
     const divFormAñadir = document.getElementById("divAñadir");
+    const divOpacar = document.getElementById("opacar");
 
-    if (!divFormAñadir.classList.contains("active")) {
+    if (!divFormAñadir.classList.contains("active") && !divOpacar.classList.contains("active")) {
         divFormAñadir.classList.add("active");
+        divOpacar.classList.add("active");
     }
+
 }
 
 function cerrarForm() {
     const divFormAñadir = document.getElementById("divAñadir");
-    if (divFormAñadir.classList.contains("active")) {
+    const divOpacar = document.getElementById("opacar");
+    if (divFormAñadir.classList.contains("active") && divOpacar.classList.contains("active")) {
         divFormAñadir.classList.remove("active");
+        divOpacar.classList.remove("active");
     }
+}
+
+
+function insertarEntrada() {
+    let fecha = document.getElementById("fechaIn").value;
+    let tipo = document.getElementById("tipoIn").value;
+    let horas = document.getElementById("horasIn").value;
+    let actividades = document.getElementById("activIn").value;
+    let observaciones = document.getElementById("obserIn").value;
+
+    let arrayInsert = [fecha, tipo, horas, actividades, observaciones];
+
+    const elementoInsertar = document.getElementById("cuerpoTabla");
+    let tr = document.createElement("tr");
+
+    for (let i = 0; i < arrayInsert.length; i++) {
+        let td = document.createElement("td");
+        td.innerText = arrayInsert[i];
+        tr.appendChild(td);
+    }
+
+    elementoInsertar.prepend(tr);
+
+}
+
+function borrarRegistro() {
+
+    let checkBoxs = document.querySelectorAll("input[type='checkbox']");
+    for (let i = 0; i < checkBoxs.length; i++) {
+        if (checkBoxs[i].checked) {
+            let idBorrar = document.getElementById(checkBoxs[i].id + "tr");
+            idBorrar.remove();
+        }
+
+    }
+
 }
